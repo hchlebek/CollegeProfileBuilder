80823a3370cf66e7c6f3e2673f21c7d69ff264d6
HChlebek <student@HChlebek.local>
2016-01-20 11:29:13 -0600

    initial commit

--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:  UIViewController, UITableViewDataSource, UITableViewDelegate
 
-    override func viewDidLoad() {
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        <#code#>
+    }
+   
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        <#code#>
     }
 
 
